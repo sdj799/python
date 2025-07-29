8766b7f07c2740652f6142162c261e7dd9c8495c
sdj799 <125543808+sdj799@users.noreply.github.com>
2025-07-29 12:48:04 +0900

    0729 클래스

---
+++ 0729.py
@@ -0,0 +1,147 @@
+# 클래스
+# 객체를 생성하기 위한 설계도
+
+# 클래스 - 붕어빵 틀
+# 객체는 - 실제 붕어빵
+# 속성(변수) - 붕어빵의 특징(크기, 맛, 색깔)
+# 매서드(함수) - 붕어빵이 할 수 있는 행동(먹히기, 식히기)
+
+class 클래명:
+    # 클래스 내용
+    pass
+
+class Person:
+    # 생성자
+    # 객체가 생성될때 자동으로 호출되는 매서드
+    # 초기 상태를 설정하는 역할
+
+    # self 키워드
+    # 현재 인스턴스 자신을 가리키는 참조
+    # 모든 인스턴스 매서드의 첫번째 매개변수
+    # 인스턴스 변수와 메서드에 접근할때 사용
+    def __init__(self, name, age):
+        self.__name = name # 인스턴스 변수
+        self.__age = age
+    
+    def introduce(self): # 인스턴스 메서드
+        print(f"안녕하세요! 제 이름은 {self.__name} 이고, {self.__age}살 입니다.")
+
+    @property
+    def name(self):
+        return self.__name
+    
+    # def getName(self):
+    #     return self.name
+    @name.setter
+    def name(self, name):
+    #def setName(self, name):
+        self.__name = name
+
+    @property
+    def age(self):
+    # def getAge(self):
+        return self.__age
+    
+    @age.setter
+    def age(self, age):
+    # def setAge(self, age):
+        self.__age = age
+    
+    
+# 객체 생성 (인스턴스화)
+person1 = Person("김철수", 25)
+person2 = Person("이영희", 20)
+
+person1.introduce()
+person2.introduce()
+
+# person1.setName("홍길동")
+# person1.setAge(30)
+# print(person1.getName())
+# print(person1.getAge())
+person1.name ="홍길동"
+person1.age = 30
+print(person1.name)
+print(person1.age)
+
+class Caculator:
+    PI = 3.14
+
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+    
+    def add(self):
+        return self.num1 + self.num2
+    
+    def sub(self):
+        return self.num1 - self.num2
+    
+    def mul(self):
+        return self.num1 * self.num2
+    
+    def div(self):
+        if(self.num2 != 0):
+            return self.num1 / self.num2
+        return 0
+    
+c = Caculator(4, 2)
+print(c.add())
+print(c.sub())
+print(c.mul())
+print(c.div())
+print(c.PI)
+
+c2 = Caculator(54, 6)
+print(c2.add())
+print(c2.sub())
+print(c2.mul())
+print(c2.div())
+print(c2.PI)
+
+class Employee:
+    serial_num = 1000
+
+    def __init__(self, name):
+        Employee.serial_num += 1
+        self.id = Employee.serial_num
+        self.name = name
+
+    def __str__(self):
+        return f"사번 : {self.id}, 이름: {self.name}"
+
+emp1 = Employee("김철수")
+emp2 = Employee("이영희")
+
+print(emp1)
+print(emp2)
+
+class Supermarket:
+    def __init__(self, location, name, product, customer):
+        self.location = location
+        self.name = name
+        self.product = product
+        self.customer = customer
+
+    def print_location(self):
+        print(f"위치: {self.location}")
+    
+    def change_category(self, product):
+        self.product = product
+
+    def show_list(self):
+        print(f"상품: {self.product}")
+
+    def enter_customer(self):
+        self.customer += 1
+    
+    def show_info(self):
+        print(f"위치: {self.location}, 이름: {self.name}, 상품: {self.product}, 고객수: {self.customer}")
+
+
+sup = Supermarket("마포구 염리동", "마켓온", "콜라", 11)
+sup.print_location()
+sup.change_category("음료")
+sup.show_list()
+sup.enter_customer()
+sup.show_info()
---
+++ 0729_2.py
@@ -0,0 +1,91 @@
+from datetime import datetime
+
+class Info:
+    def __init__(self, breed):
+        self._breed = breed
+
+class Animal:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    def speak(self):
+        return f"동물 소리를 냅니다."
+
+    def move(self):
+        return f"동물이 움직입니다."
+
+class Dog(Animal, Info):
+    def __init__(self, name, age, breed):
+        # 슈퍼 클래스(부모 클래스) 생성자 호출
+        Animal.__init__(self, name, age)
+        Info.__init__(self, breed)
+        # self.breed = breed
+
+    # 매서드 오버라이딩
+    def speak(self):
+        # return super().speak()
+        return f"{self._name}: 멍멍!"
+    
+    # 서브 클래스 고유 매서드
+    def tail(self):
+        return f"{self._name}이(가) 꼬리를 흔듭니다!"
+print("=========== 상속 ==========") 
+dog = Dog("멍멍이", 3, "진돗개")
+print(dog.move()) # 부모 클래스
+print(dog.speak()) # 오버라이딩
+print(dog.tail()) # 자식 클래스
+
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def update_quantity(self, amount):
+        self.quantity += amount
+        print(f"{self.name} 재고가 {amount}만큼 {'증가' if amount > 0 else '감소'}했습니다. 현재 재고: {self.quantity}")
+
+    def display_info(self):
+        print(f"상품명: {self.name}")
+        print(f"가격: {self.price}원")
+        print(f"재고: {self.quantity}개")
+
+class Electronic(Product):
+    def __init__(self, name, price, quantity, warranty_period = 12):
+        super().__init__(name, price, quantity)
+        self.warranty_period = warranty_period
+
+    def extend_warranty(self, months):
+        self.warranty_period += months
+        print(f"보증기간이 {months}개월 연장되었습니다. 현재 보증 기간: {self.warranty_period}개월")
+
+    def display_info(self):
+        super().display_info()
+        print(f"보증기간: {self.warranty_period}개월")
+
+class Food(Product):
+    def __init__(self, name, price, quantity, expiration_date):
+        super().__init__(name, price, quantity)
+        self.expiration_date = expiration_date
+    
+    def is_expired(self, current_date):
+        exp_date = datetime.strptime(self.expiration_date, "%Y-%m-%d")
+        curr_date = datetime.strptime(current_date, "%Y-%m-%d")
+        if curr_date > exp_date:
+            print(f"{self.name}는 유통기한이 지났습니다")
+        else:
+            print(f"{self.name}는 유통기한이 지나지 않았습니다")
+
+
+    def display_info(self):
+        super().display_info()
+        print(f"유통기한: {self.expiration_date}")
+    
+tv = Electronic("스마트 TV", 1500000, 5, 24)
+tv.extend_warranty(12)
+tv.display_info()
+
+apple = Food("사과", 3000, 50, "2025-07-28")
+apple.is_expired("2025-07-29")
+apple.display_info()
